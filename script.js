e673451d0bb88e0b49aa614034050a40350083c2
SofiiaNykon <sofianikonovic@gmail.com>
2024-05-09 17:32:37 +0200

    added Login valution

---
+++ script.js
@@ -0,0 +1,12 @@
+const number = document.getElementById("number");
+const firstConversion = document.getElementById("celsius");
+const secondConversion = document.getElementById("fahrenheit");
+const button = document.getElementById("submit");
+const result = document.getElementById("result");
+
+button.oncklick = function(){
+    if(firstConversion.onclick) {
+return
+    }
+    
+}
