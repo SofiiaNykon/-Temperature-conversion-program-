8cbde3e9b3fae6cc0e6fd550e1d3780a34fb8765
SofiiaNykon <sofianikonovic@gmail.com>
2024-05-12 12:44:44 +0200

    css style

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function convert(){
     if(conversionToF.checked){
     num=Number(number.value);
     num = num * 9 / 5 + 32;
-    result.textContent = num.toFixed(1) + "F";}
+    result.textContent = num.toFixed(1) + " °F";}
     else if(conversionToC.checked){
         num=Number(number.value);
         num = (num - 32) * 5 / 9;
-        result.textContent = num.toFixed(1) + "C";}
+        result.textContent = num.toFixed(1) + " °C";}
       else{  result.textContent = "You need to choose value";
     
     }};
