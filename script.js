5a2f3cfc65f57294703dde0921f21cb3506939e7
SofiiaNykon <sofianikonovic@gmail.com>
2024-05-17 23:17:35 +0200

    smaller function

--- script.js
+++ script.js
@@ -3,17 +3,24 @@ const conversionToC = document.getElementById("toCelsius");
 const conversionToF = document.getElementById("toFahrenheit");
 const result = document.getElementById("result");
 let num;
-function convert(){
-  num=Number(number.value);
-    if(conversionToF.checked) num = num * 9 / 5 + 32;
-      answer = result.textContent = num.toFixed(1) + " °F";
-     if(conversionToC.checked) num = (num - 32) * 5 / 9;
-          result.textContent = num.toFixed(1) + " °C";
-       return "You need to choose value";}
+
+convert = (num) =>{num=Number(number.value); if(conversionToF.checked) num = num * 9 / 5 + 32;
+result.textContent = num.toFixed(1) + ` °F`;
+  if(conversionToC.checked) num = (num - 32) * 5 / 9; result.textContent = num.toFixed(1) + ` °C`;
+   return `You need to choose value`}
 
 
+// function convert(){
+//   num=Number(number.value);
+//     if(conversionToF.checked) num = num * 9 / 5 + 32;
+//       answer = result.textContent = num.toFixed(1) + " °F";
+//      if(conversionToC.checked) num = (num - 32) * 5 / 9;
+//           result.textContent = num.toFixed(1) + " °C";
+//        return "You need to choose value";}
 
 
+// function convert(){
+//   num=Number(number.value);
     // if(conversionToF.checked){
     // num = num * 9 / 5 + 32;
     // result.textContent = num.toFixed(1) + " °F";}
