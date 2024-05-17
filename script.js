a5cd8b615bc052bd148076755ab6b33aeaa03912
SofiiaNykon <sofianikonovic@gmail.com>
2024-05-17 23:00:37 +0200

    another function

--- script.js
+++ script.js
@@ -4,15 +4,25 @@ const conversionToF = document.getElementById("toFahrenheit");
 const result = document.getElementById("result");
 let num;
 function convert(){
+  num=Number(number.value);
+    if(conversionToF.checked) num = num * 9 / 5 + 32;
+      answer = result.textContent = num.toFixed(1) + " °F";
+     if(conversionToC.checked) num = (num - 32) * 5 / 9;
+          result.textContent = num.toFixed(1) + " °C";
+       return "You need to choose value";}
 
-    if(conversionToF.checked){
-    num=Number(number.value);
-    num = num * 9 / 5 + 32;
-    result.textContent = num.toFixed(1) + " °F";}
-    else if(conversionToC.checked){
-        num=Number(number.value);
-        num = (num - 32) * 5 / 9;
-        result.textContent = num.toFixed(1) + " °C";}
-      else{  result.textContent = "You need to choose value";
+
+
+
+    // if(conversionToF.checked){
+    // num = num * 9 / 5 + 32;
+    // result.textContent = num.toFixed(1) + " °F";}
+    // else if(conversionToC.checked){
+    //     num = (num - 32) * 5 / 9;
+    //     result.textContent = num.toFixed(1) + " °C";}
+    //   else{  result.textContent = "You need to choose value";
     
-    }};
+    // }};
+
+ 
+
